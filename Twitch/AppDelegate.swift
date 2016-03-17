d52276d0fe90bf31c8f510eb8c84a7118c3d48d3
Adam Holt <omgitsads@github.com>
2016-03-17 21:00:22 +0100

    Initial Commit

---
+++ Twitch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Twitch
+//
+//  Created by Adam Holt on 17/03/2016.
+//  Copyright © 2016 Adam Holt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Twitch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Twitch
+//
+//  Created by Adam Holt on 17/03/2016.
+//  Copyright © 2016 Adam Holt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
