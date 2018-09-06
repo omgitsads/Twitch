6c2d16614ba053944bca9c03eddecc8b9cadd8a5
Adam Holt <omgitsads@github.com>
2018-09-06 09:05:51 +0200

    Initial Commit

---
+++ Twitch/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Twitch
+//
+//  Created by Adam Holt on 06.09.18.
+//  Copyright © 2018 Adam Holt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
